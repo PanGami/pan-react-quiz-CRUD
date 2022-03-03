808695e69f3a7ccfecbd3f1ae66dd5d96ea0301b
Panca Nugraha Wicaksana <pancanugrahawicaksana@gmail.com>
2022-03-03 15:36:18 +0700

    Timer not fixed

--- client/src/pages/Quiz.js
+++ client/src/pages/Quiz.js
@@ -5,7 +5,49 @@ import Results from '../Components/Results'
 import '../styles/quiz.css'
 import { createQuizData as quizData } from '../api/opentdb'
 
+const Timer = (props) => {
+  const { initMinute = 0, initSeconds = 10 } = props
+  const [minutes, setMinutes] = React.useState(initMinute)
+  const [seconds, setSeconds] = React.useState(initSeconds)
+
+  React.useEffect(() => {
+    let myInterval = setInterval(() => {
+      if (seconds > 0) {
+        setSeconds(seconds - 1)
+      } if (seconds === 0) {
+        if (minutes === 0) {
+          clearInterval(myInterval)
+        } else {
+          setMinutes(minutes - 1)
+          setSeconds(59)
+        }
+      }
+    }, 1000)
+    return () => {
+      clearInterval(myInterval)
+    }
+  })
+
+  return (
+    <React.Fragment>
+			<div className='wrapper'>
+				{ minutes === 0 && seconds === 0 ? (
+          <div className='time-up'>
+            <h1>Time's Up!</h1>
+            <h2>You're done with the quiz!</h2>
+          </div>
+				) : (
+					<React.Fragment>
+						<h1>{minutes < 10 ? `0${minutes}` : minutes}:{seconds < 10 ? `0${seconds}` : seconds}</h1>
+					</React.Fragment>
+				)}
+			</div>
+    </React.Fragment>
+  )
+}
+
 class App extends Component {
+  
   constructor(props) {
     super(props)
     this.state = {
@@ -34,6 +76,7 @@ class App extends Component {
   }
 
   render() {
+    
     if (this.state.loading === false) {      
       if (this.state.current >= this.state.questions.length) {
         var scorebox = ''
@@ -43,13 +86,14 @@ class App extends Component {
         results = ''
       }
       return (
-        <div>
+        <div>          
           {scorebox}
           <QuestionList
             {...this.state}
             setCurrent={this.setCurrent.bind(this)}
             setScore={this.setScore.bind(this)}
           />
+           <Timer initMinute={3} initSeconds={30} />
           {results}
         </div>
       )
